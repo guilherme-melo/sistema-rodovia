857dd984e4178b5e85245c35f639f01cf8940592
felipelmc <felipe.lamarca@hotmail.com>
2023-05-16 14:17:42 -0300

    Now the server populates the database

--- rpc/server.py
+++ rpc/server.py
@@ -1,14 +1,30 @@
 import grpc
 import rpc_pb2
 import rpc_pb2_grpc
-
+from pymongo import MongoClient
 from concurrent import futures
+import json
+
+SERVER_URL = 'mongodb://localhost:27017'
+DB_NAME = 'etl'
+COLLECTION_NAME = 'car-data'
+
+def connect_database(server_url, db_name, collection_name):
+    client = MongoClient(server_url)
+    db = client[db_name]
+    return db[collection_name]
+
+connection = connect_database(SERVER_URL, DB_NAME, COLLECTION_NAME)
 
 class RoadSimServicer(rpc_pb2_grpc.RoadSimServicer):
     def Simulate(self, request, context):
         response = rpc_pb2.Response()
         response.message = f"{request.name}"
-        print(response.message, '\n')
+        json_message = json.loads(response.message)
+        cursor = connection.insert_one(json_message)
+        print(cursor.inserted_id)
+        
+        #print(response.message, '\n')
         return response
 
 def serve():
