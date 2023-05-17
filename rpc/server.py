930458c841a8530ce418d0ce4b3bebd44c4ec102
felipelmc <felipe.lamarca@hotmail.com>
2023-05-17 19:57:27 -0300

    Sends each mock to its own collection in MongoDB

--- rpc/server.py
+++ rpc/server.py
@@ -7,24 +7,29 @@ import json
 
 SERVER_URL = 'mongodb://localhost:27017'
 DB_NAME = 'etl'
-COLLECTION_NAME = 'car-data'
 
-def connect_database(server_url, db_name, collection_name):
-    client = MongoClient(server_url)
-    db = client[db_name]
-    return db[collection_name]
-
-connection = connect_database(SERVER_URL, DB_NAME, COLLECTION_NAME)
+client = MongoClient(SERVER_URL)
+db = client[DB_NAME]
 
 class RoadSimServicer(rpc_pb2_grpc.RoadSimServicer):
     def Simulate(self, request, context):
         response = rpc_pb2.Response()
         response.message = f"{request.name}"
         json_message = json.loads(response.message)
+        
+        # get the avenue name
+        avenue_name = json_message['avenue']
+        COLLECTION_NAME = avenue_name
+        
+        collection_names = db.list_collection_names()
+
+        if COLLECTION_NAME not in collection_names:
+            print("Creating collection: ", COLLECTION_NAME)
+            db.create_collection(COLLECTION_NAME)
+        
+        connection = db[COLLECTION_NAME]
         cursor = connection.insert_one(json_message)
         print(cursor.inserted_id)
-        
-        #print(response.message, '\n')
         return response
 
 def serve():
